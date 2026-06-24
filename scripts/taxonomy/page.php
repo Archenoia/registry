4b557618d2375721ff0a20f16293a2ed3dde884e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-24 09:24:03 +0800

    split source function into multiple parts

--- scripts/taxonomy/page.php
+++ scripts/taxonomy/page.php
@@ -1,33 +1,41 @@
 <?php
 
 class ncbi_taxonomy {
-SELECT 
-    mainclass.term AS main_class,
-    subclass.term AS sub_class,
-    trait_name.term AS trait_name,
-    unit,
-    consensus_value,
-    min,
-    median,
-    mean,
-    max,
-    discrete_values,
-    ontology_ids
-FROM
-    cad_registry.organism_traits
-        LEFT JOIN
-    ontology trait_name ON trait_name.id = traits_id
-        LEFT JOIN
-    ontology_relation is_subclass ON is_subclass.term_id = trait_name.id
-        LEFT JOIN
-    ontology subclass ON subclass.id = is_subclass.is_a
-        LEFT JOIN
-    ontology_relation is_mainclass ON is_mainclass.term_id = subclass.id
-        LEFT JOIN
-    ontology mainclass ON mainclass.id = is_mainclass.is_a
-WHERE
-    tax_id = 46125
-ORDER BY main_class , sub_class , trait_name
+
+    public static function organism_traits($taxid) {
+        $traits = new Table(["cad_registry"=>"organism_traits"]);
+        $traits = $traits->getDriver()->Fetch("SELECT 
+                mainclass.term AS main_class,
+                subclass.term AS sub_class,
+                trait_name.term AS trait_name,
+                unit,
+                consensus_value,
+                min,
+                median,
+                mean,
+                max,
+                discrete_values,
+                ontology_ids
+            FROM
+                cad_registry.organism_traits
+                    LEFT JOIN
+                ontology trait_name ON trait_name.id = traits_id
+                    LEFT JOIN
+                ontology_relation is_subclass ON is_subclass.term_id = trait_name.id
+                    LEFT JOIN
+                ontology subclass ON subclass.id = is_subclass.is_a
+                    LEFT JOIN
+                ontology_relation is_mainclass ON is_mainclass.term_id = subclass.id
+                    LEFT JOIN
+                ontology mainclass ON mainclass.id = is_mainclass.is_a
+            WHERE
+                tax_id = {$taxid}
+            ORDER BY main_class , sub_class , trait_name;
+        ");
+
+        return $traits;
+    }
+
     public static function taxon_data($id,$page=1,$page_size = 35) {
         $id = Regex::Match($id, "\d+");
         
@@ -48,7 +56,6 @@ ORDER BY main_class , sub_class , trait_name
         $tax["parent_name"] = $parent["name"];
         $tax["parent_rank"] = $parent["rank_name"];
         $childs = json_decode($tax["childs"], true);
-        $prot_offset = ($page-1) * $page_size;
 
         if (!Utils::isDbNull($childs)) {
             if (count($childs) > 0) {
@@ -56,6 +63,7 @@ ORDER BY main_class , sub_class , trait_name
                     ->left_join("vocabulary")
                     ->on(["ncbi_taxonomy"=>"rank","vocabulary"=>"id"])
                     ->where(["`ncbi_taxonomy`.id"=>in($childs)])
+                    ->limit(10)
                     ->select(["ncbi_taxonomy.*","term as rank_name"])
                     ;
                 $tax["childs"] = $childs;
@@ -66,36 +74,52 @@ ORDER BY main_class , sub_class , trait_name
             $tax["childs"] = [];
         }
     
+        
+        $tax["enzyme"] = self::organism_proteins($id, $page, $page_size);
+        $tax["metabolite"] = self::organism_metabolites($id);
+
+        return list_nav( $tax,$page);
+    }
+
+    public static function organism_proteins($taxid, $page =1, $page_size = 35) {
+        $prot_offset = ($page-1) * $page_size;
         $prot_data = FASTA_PROTEIN;
         $ec_number = EC_NUMBER;
         $sql = "SELECT 
-        protein_data.id,
-        source_id,
-        source_db,
-        term as db_name,
-        name,
-        `function`,
-        db_xref AS ec_number
-    FROM
-        cad_registry.protein_data
-            LEFT JOIN
-        db_xrefs ON db_xrefs.obj_id = protein_data.id
-            AND db_xrefs.type = {$prot_data}
-            AND db_xrefs.db_name = {$ec_number}
-            LEFT JOIN
-        vocabulary ON vocabulary.id = protein_data.source_db
-    WHERE
-        ncbi_taxid = {$id}
-            AND NOT db_xref IS NULL
-    LIMIT {$prot_offset},{$page_size}
-            ";
-        $tax["enzyme"] = (new Table(["cad_registry"=>"protein_data"]))->exec($sql);
-        $tax["metabolite"] = (new Table(["cad_registry"=>"organism_source"]))
+            protein_data.id,
+            source_id,
+            source_db,
+            term as db_name,
+            name,
+            `function`,
+            db_xref AS ec_number
+        FROM
+            cad_registry.protein_data
+                LEFT JOIN
+            db_xrefs ON db_xrefs.obj_id = protein_data.id
+                AND db_xrefs.type = {$prot_data}
+                AND db_xrefs.db_name = {$ec_number}
+                LEFT JOIN
+            vocabulary ON vocabulary.id = protein_data.source_db
+        WHERE
+            ncbi_taxid = {$taxid}
+                AND NOT db_xref IS NULL
+        LIMIT {$prot_offset},{$page_size}
+        ";
+
+        return (new Table(["cad_registry"=>"protein_data"]))->exec($sql);
+    }
+
+    public static function organism_metabolites($taxid, $page = 1, $page_size = 100) {
+        $offset = ($page -1) * $page_size;
+
+        return (new Table(["cad_registry"=>"organism_source"]))
             ->left_join("metabolites")
             ->on(["metabolites"=>"id","organism_source"=>"metabolite_id"])
             ->left_join("struct_data")
             ->on(["struct_data"=>"metabolite_id","metabolites"=>"id"])
-            ->where(["organism_id"=>$id])
+            ->where(["organism_id"=>$taxid])
+            ->limit($offset, $page_size)
             ->select(["`metabolites`.id",
                 "evidence",
                 "name",
@@ -107,8 +131,6 @@ ORDER BY main_class , sub_class , trait_name
                 "biocyc",
                 "smiles"])
             ;
-
-        return list_nav( $tax,$page);
     }
 
     private static function find_tax($q) {
