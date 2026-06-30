3434865f5be63ea35066d06a0d17da6adeda88fe
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-01 06:13:52 +0800

    add translation text

---
+++ views/index.enUS.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "arche" => "Arch<span style='color:lightgray;'>a</span>e<span style='color:lightgray;'>a</span>, ",
+    "noia" => "noia",
+    "intro_text" => "Focused on microbial life systems and natural product synthesis. It integrates multi-dimensional data ranging from genomes and proteomes to metabolic networks, molecular docking, and mass spectra. It is dedicated to providing precise and efficient data support for virtual cell modeling and metabolic pathway analysis software, empowering systems biology research, and accelerating life science discoveries."
+];
---
+++ views/index.frFR.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "arche" => "Arch<span style='color:lightgray;'>a</span>e<span style='color:lightgray;'>a</span>, ",
+    "noia" => "noia",
+    "intro_text" => "Concentré sur les systèmes vivants microbiens et la synthèse de produits naturels. Il intègre des données multidimensionnelles allant des génomes et des protéomes aux réseaux métaboliques, à l’amarrage moléculaire et aux spectres de masse. Il est dédié à fournir un support de données précis et efficace pour les logiciels de modélisation cellulaire virtuelle et d’analyse des voies métaboliques, afin de stimuler la recherche en biologie des systèmes et d’accélérer les découvertes en sciences du vivant."
+];
